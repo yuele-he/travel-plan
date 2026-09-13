edd8822a167ac1edd4762c28f09704aa463ca3dd
He Yuele <103713648+yuele-he@users.noreply.github.com>
2026-09-13 09:32:22 -0700

    Use publicly served data bundle path

--- src/data.js
+++ src/data.js
@@ -1,3 +1,3 @@
 import { loadGzipParts } from './load-gzip-parts.js';
-const mod = await loadGzipParts(new URL('./_parts/data.gz', import.meta.url).href, 3);
+const mod = await loadGzipParts(new URL('./parts/data.gz', import.meta.url).href, 3);
 export const DATA = mod.DATA;
